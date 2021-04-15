616dab348e5c25363a5caace4e08114b4972672d
b_stef <stephane.betton@gmail.com>
2021-04-15 21:15:40 +0200

    first

--- src/main/java/com/stef/rh/repository/CountriesRepository.java
+++ src/main/java/com/stef/rh/repository/CountriesRepository.java
@@ -3,7 +3,9 @@ package com.stef.rh.repository;
 import com.stef.rh.entity.Countries;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CountriesRepository extends JpaRepository< Countries, String >, JpaSpecificationExecutor< Countries > {
 
 }
--- src/main/java/com/stef/rh/service/IRegionsService.java
+++ src/main/java/com/stef/rh/service/IRegionsService.java
@@ -1,4 +1,7 @@
 package com.stef.rh.service;
 
+import java.util.List;
+
 public interface IRegionsService {
+    List< com.stef.rh.models.RegionsDto > getAllRegions();
 }
