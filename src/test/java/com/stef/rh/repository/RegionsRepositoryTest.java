cb9b965661bd84c956c6598d1a736d5cc65d6d20
b_stef <stephane.betton@gmail.com>
2021-05-12 21:34:40 +0200

    add H2 database pour les TU

--- src/test/java/com/stef/rh/repository/RegionsRepositoryTest.java
+++ src/test/java/com/stef/rh/repository/RegionsRepositoryTest.java
@@ -1,15 +1,19 @@
 package com.stef.rh.repository;
 
 import com.stef.rh.entity.Regions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-
+@ExtendWith(SpringExtension.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class RegionsRepositoryTest {
@@ -17,7 +21,6 @@ class RegionsRepositoryTest {
     RegionsRepository repository;
 
     @Test
-    @Disabled
     void testSave() {
         Regions regions = new Regions();
         regions.setRegionName("bitter");
@@ -25,7 +28,16 @@ class RegionsRepositoryTest {
         Regions value = repository.save(regions);
         assertThat(value.getRegionId()).isNotNull();
         assertThat(value.getRegionName()).isEqualTo("bitter");
-
     }
 
+    @Test
+    @Sql("/sql/createRegions.sql")
+    void testFindById() {
+        Optional< Regions > value = repository.findById(1L);
+        assertThat(value).isPresent()
+                .containsInstanceOf(Regions.class);
+        Regions regions = value.get();
+        assertThat(regions.getRegionId()).isNotNull();
+        assertThat(regions.getRegionName()).isEqualTo("Europe");
+    }
 }
---
+++ src/test/resources/sql/createRegions.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.regions (region_id, region_name) VALUES(1, 'Europe');
+INSERT INTO public.regions (region_id, region_name) VALUES(2, 'Americas');
